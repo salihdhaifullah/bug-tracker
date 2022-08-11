5d6041fca690dc849ce26744438ebaea04119207
salehWeb <hsn220488@gmail.com>
2022-08-11 12:05:32 -0700

    made RestFul api for files

--- server/Controllers/FilesController.cs
+++ server/Controllers/FilesController.cs
@@ -74,7 +74,7 @@ namespace server.Controllers
                 var newFille = new Fille
                 {
                     type = type == "png" || type == "svg" || type == "image" || type == "jpeg" || type == "jpg" ? "Image" : "Document",
-                    name = file.FileName,
+                    name = fileNameForShaft,
                     Description = Description,
                     TicketId = ticketId,
                     CreatorId = (int)id,
@@ -131,7 +131,75 @@ namespace server.Controllers
             file.Description = Description;
 
             await _context.SaveChangesAsync();
-            return Ok();
+            return Ok(file);
+        }
+
+
+        [HttpDelete("{id}"), Authorize]
+        public async Task<IActionResult> DeleteFile([FromRoute] int id)
+        {
+            var file = await _context.Filles.FirstOrDefaultAsync(file => file.Id == id);
+
+            if (file == null) return NotFound();
+            string? header = Request.Headers.Authorization;
+
+            string[] token = header.Split(' ');
+
+            var UserId = _token.VerifyToken(token[1]);
+
+            if (UserId == null) return Unauthorized();
+
+            if (file.CreatorId != (int)UserId) return Unauthorized();
+
+            var API = new FirebaseAuthProvider(new FirebaseConfig(_configuration.GetSection("FireBase:apiKay").Value));
+
+            var user = await API.SignInWithEmailAndPasswordAsync(
+            _configuration.GetSection("Admin:Email").Value,
+            _configuration.GetSection("Admin:Password").Value
+            );
+
+            var DeleteFile = new FirebaseStorage(
+                _configuration.GetSection("FireBase:storageBucket").Value,
+                new FirebaseStorageOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(user.FirebaseToken),
+                    ThrowOnCancel = true
+                }
+            ).Child("images")
+             .Child($"{file.name}")
+             .DeleteAsync();
+
+            try
+            {
+                await DeleteFile;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest("sorry something went wrong 😥");
+            }
+
+            _context.Filles.Remove(file);
+            await _context.SaveChangesAsync();
+            return Ok("deleted");
+        }
+
+        [HttpGet("{id}"), Authorize]
+        public async Task<IActionResult> GetFile([FromRoute] int id)
+        {
+            var file = await _context.Filles.FirstOrDefaultAsync(file => file.Id == id);
+
+            if (file == null) return NotFound();
+
+            return Ok(file);
+        }
+
+        [HttpGet("Ticket/{TicketId}"), Authorize]
+        public async Task<IActionResult> GetFiles([FromRoute] int TicketId)
+        {
+            var files = await _context.Filles.Where(files => files.TicketId == TicketId).ToListAsync();
+
+            return Ok(files);
         }
     }
 }
