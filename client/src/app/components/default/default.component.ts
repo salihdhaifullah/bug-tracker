aa89ecc6bb2dc3a2a65adbe007b6b34dbb726682
salehWeb <hsn220488@gmail.com>
2022-10-03 07:45:11 -0700

    solve bug NG0100

--- client/src/app/components/default/default.component.ts
+++ client/src/app/components/default/default.component.ts
@@ -1,26 +1,24 @@
 import { Store } from '@ngrx/store';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { BreakpointObserver} from '@angular/cdk/layout'
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout'
 import { MatSidenav } from '@angular/material/sidenav';
 import { IAppState } from 'src/context/app.state';
 import * as Actions from 'src/context/actions';
-import {Router} from "@angular/router"
+import { Router } from "@angular/router"
 import Swal from 'sweetalert2';
-import { AuthService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
 })
-export class DefaultComponent implements AfterViewInit {
-  @ViewChild(MatSidenav) sidenav! : MatSidenav;
- 
+export class DefaultComponent implements OnInit {
 
-  constructor(private authService: AuthService, private observer : BreakpointObserver, private store: Store<IAppState>, private router: Router) { }
+  isClosed = true;
+  @ViewChild(MatSidenav, {static:true}) sidenav!: MatSidenav;
+  constructor(private observer: BreakpointObserver, private store: Store<IAppState>, private router: Router) { }
 
 
   HandelLogout(): void {
-    
     Swal.fire({
       title: 'Logout',
       text: 'do You want to logout',
@@ -29,32 +27,30 @@ export class DefaultComponent implements AfterViewInit {
       showCancelButton: true,
     }).then((result) => {
       if (result.value) {
-          localStorage.clear();
-          this.store.dispatch(Actions.Logout());
-          this.router.navigate(['/login']);
+        localStorage.clear();
+        this.store.dispatch(Actions.Logout());
+        this.router.navigate(['/login']);
       }
-    }
-  )
+    });
+  };
 
-  }
 
-  isClosed = true;
   handelClick(): void {
     this.sidenav.toggle();
     this.isClosed = !this.isClosed;
   }
-  ngAfterViewInit(): void {
 
-    this.observer.observe(['(max-width: 800px)']).subscribe((res)=>{
-      if(res.matches){
-        this.sidenav.mode = 'over';
-        this.sidenav.close()
-      }
-      else{
-        this.sidenav.mode = 'side';
-        this.sidenav.open()
-      }
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close()
+        }
+        else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open()
+        }
+        console.log("yub");
     })
   }
-   
 }
--- client/src/app/components/user-ticket/user-ticket.component.ts
+++ client/src/app/components/user-ticket/user-ticket.component.ts
@@ -6,6 +6,8 @@ import { IAppState } from 'src/context/app.state';
 import { Store, select } from '@ngrx/store';
 import { ITicket } from 'src/types/Tickets';
 import { devTicketSelector } from 'src/context/selectors';
+import { Static } from 'src/Static';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-user-ticket',
@@ -15,37 +17,77 @@ import { devTicketSelector } from 'src/context/selectors';
 export class UserTicketComponent implements OnInit {
   isChanged: boolean
   devTickets$: Observable<ITicket[]>;
-  devTickets: ITicket[] | never[];
-  
+  devTickets: ITicket[];
+  New: ITicket[];
+  Progress: ITicket[];
+  Done: ITicket[];
+
   constructor(private store: Store<IAppState>) {
     this.isChanged = false
     this.devTickets = [];
+    this.New = [];
+    this.Progress = [];
+    this.Done = [];
     this.devTickets$ = this.store.pipe(select(devTicketSelector));
   }
 
   ngOnInit(): void {
     this.store.dispatch(Actions.getDevTickets());
-    this.devTickets$.subscribe((data: any) => this.devTickets = data.devTickets);
-    console.log(this.devTickets);
+    this.getTicketsAndFilterThem();
   }
 
 
   HandleSave() {
-    console.log("HandleSave");
+    console.log(this.New);
+    console.log(this.Progress);
+    console.log(this.Done);
+    
+    Swal.fire({
+      title: 'do you',
+      text: 'do you want to save change',
+      icon: 'warning',
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.value) {
+      // do database stuff 
+      } else this.filterTicket(); // rest to database current values
+      
+    });
   }
 
-  New = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  Progress = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
-  Done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<ITicket[]>) {
     this.isChanged = true;
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
+
+
+  getTicketsAndFilterThem(): void {
+    this.devTickets$.subscribe((data: any) => {
+      if (data.devTickets) {
+        this.devTickets = data.devTickets;
+        this.filterTicket();
+      }
+    });
+  }
+
+  filterTicket(): void {
+    if (this.New.length !== 0) this.New = [];
+    if (this.Progress.length !== 0) this.Progress = [];
+    if (this.Done.length !== 0) this.Done = [];
+
+    if (this.devTickets.length >= 1) {
+      for (let devTicket of this.devTickets) {
+        if (devTicket.status === Static.Statuses.New) this.New.push(devTicket);
+        else if (devTicket.status === Static.Statuses.InProgress) this.Progress.push(devTicket);
+        else if (devTicket.status === Static.Statuses.Closed) this.Done.push(devTicket);
+        else throw new Error("data error")
+      }
     }
   }
 
