4addef2a2d3c7dcbd8bc64588af5e5bac13d2630
salehWeb <salehwebdev2004@gmail.com>
2023-05-12 22:12:43 +0300

    reusable textfield component

--- Client/src/components/utils/TextFiled.tsx
+++ Client/src/components/utils/TextFiled.tsx
@@ -1,24 +1,83 @@
+import { useEffect, useState } from "react";
 import NewId from "../../utils/NewId";
+import { IconType } from "react-icons";
 
-interface TextFiledProps {
-    type: React.HTMLInputTypeAttribute | undefined
-    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
-    value: string | undefined
-    label: string
+const LABEL_FOCUS = "bottom-[95%] left-[12%] text-sm text-secondary";
+const LABEL = "text-base bottom-[20%] left-[20%] text-gray-600";
+const Id = NewId();
+
+export interface IValidate {
+    validate: (str: string) => boolean;
+    massage: string
 }
 
+interface TextFiledProps {
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    value: string
+    label: string
+    isValid: React.MutableRefObject<boolean>
+    type?: React.HTMLInputTypeAttribute
+    validation?: IValidate[]
+    icon?: IconType
+}
+
+
 const TextFiled = (props: TextFiledProps) => {
-    const Id = NewId();
+
+    const [isFocus, setIsFocus] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [changing, setChanging] = useState(false);
+    const [errorMassage, setErrorMassage] = useState("");
+    const [labelClassName, setLabelClassName] = useState("absolute z-10 font-extralight transition-all ease-in-out text-base bottom-[20%] left-[20%] text-gray-600");
+
+    useEffect(() => {
+        if (props.value) setLabelClassName("sr-only");
+        else setLabelClassName(`absolute z-10 font-extralight transition-all ease-in-out  ${isFocus ? LABEL_FOCUS : LABEL}`);
+    }, [isFocus, changing])
+
+    const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.onChange(e)
+
+        setIsFocus(true)
+        setIsError(false)
+        setChanging(!changing)
+
+        if (
+            !props?.validation ||
+             props?.validation.length === 0 ||
+            !e.target.value
+            ) return;
+
+        for (let i = 0; i < props?.validation.length; i++) {
+            const item = props?.validation[0];
+            if (!item.validate(e.target.value)) {
+                setErrorMassage(item.massage)
+                setIsError(true)
+                break;
+            }
+        }
+    }
 
     return (
-        <div>
-            <label htmlFor={Id}>{props.label}</label>
-            <input
-                id={Id}
-                type={props?.type}
-                onChange={props?.onChange}
-                value={props?.value}
-            />
+        <div className="flex flex-col justify-center items-center p-2 px-6 w-full gap-2">
+            <div className="flex flex-row gap-2 w-full justify-center items-center relative">
+                <label
+                    htmlFor={Id}
+                    className={labelClassName}>
+                    {props.label}
+                </label>
+                {!props?.icon ? null : <props.icon className="text-gray-600 text-xl font-bold"/>}
+                <input
+                    className={`p-2 border h-fit rounded-sm w-full ${isError ? "border-red-500 hover:border-red-700 focus:outline-red-600" : "border-gray-400 hover:border-gray-900 focus:outline-secondary"} `}
+                    id={Id}
+                    type={props?.type}
+                    value={props?.value}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                    onChange={handelChange}
+                />
+            </div>
+            {!isError ? null : <p className="text-red-600 text-center text-base font-bold">{errorMassage}</p>}
         </div>
     )
 }
--- Client/src/pages/auth/Login.tsx
+++ Client/src/pages/auth/Login.tsx
@@ -1,4 +1,13 @@
+import { useRef, useState } from "react";
+import TextFiled from "../../components/utils/TextFiled"
+import { MdEmail } from "react-icons/md";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const isValidEmail = useRef(true);
+  const isValidPassword = useRef(true);
+
   return (
     <section className="flex flex-col justify-center items-center flex-grow">
       <div className="rounded-xl bg-white flex flex-col gap-4 w-80 p-2 items-center justify-center shadow-xl">
@@ -18,6 +27,32 @@ const Login = () => {
               <input className="p-2 border border-gray-400 h-fit hover:border-gray-900 focus:outline-secondary rounded-sm w-full" type="text" id="Email" value="@Model.Email" name="Email" />
             </div>
           </div>
+          <TextFiled
+            validation={[
+              {
+                validate: (str: string) => str.length >= 10,
+                massage: "unValved Email Address"
+              }
+            ]}
+            icon={MdEmail}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="email address"
+            isValid={isValidEmail}
+          />
+
+          <TextFiled
+            validation={[
+              {
+                validate: (str: string) => str.length >= 10,
+                massage: "unValved password"
+              }
+            ]}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="password"
+            isValid={isValidPassword}
+          />
 
           <div className="flex flex-col justify-center items-center p-2 px-6 w-full gap-2">
             <div className="flex flex-row gap-2  w-full justify-center items-center relative">
